73fdececfbe03f25afaca3ee977c8035954f8ba1
夏花 生如 <w528085171@outlook.com>
2022-04-30 22:10:08 +0800

    shouldnt use appcontext by safe policy...2022年04月30日 22:10:08

--- lib/io.ts
+++ lib/io.ts
@@ -1,14 +1,13 @@
 import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
-export type field = Array<string>;
-export type RawFile={data:string,content:string}
-type postsData = {
-    'title'?: string,
-    'author'?: string,
-    'description'?: string,
-    'content'?: string,
-}
+
+export type PostData = {
+    title: string,
+    content: string,
+    brief?:string,
+    author:string
+  }
 const postsDirectory = join(process.cwd(), '_posts')
 
 export function getRawFileByPath(fileName = 'helloworld') {
